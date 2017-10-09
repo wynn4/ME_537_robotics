8679b688dd00a68f71eef87a608f67c8a4979b4c
Jesse Wynn <wynnsquad@gmail.com>
2017-10-09 16:49:03 -0600

    added the rest of hw_3

--- hw_3/p1_d.m
+++ hw_3/p1_d.m
@@ -5,3 +5,14 @@ clc
 % clear
 
 % Problem 8-2 from the book
+
+% for the puma560 robot, can you devise an situation where three axes are
+% parallel?
+
+mdl_puma560
+bot = p560;
+
+q = [0 0 0 0 0 0];
+bot.plot(q)
+
+disp 'yes.  In the zero-angle pose, axes q2,q3, and q5 are parallel'
